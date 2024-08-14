95657b76c4682c173322d3c4309ca06e8ac6bac3
Patricio9991 <patriciopucheta99@gmail.com>
2024-08-14 14:10:02 -0300

    gettin ready for render

--- backend/src/index.js
+++
@@ -1,7 +0,0 @@
-import server from './server.js'
-import { connectDB } from './db.js'
-
-
-connectDB()
-
-
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,11 +4,14 @@ import morgan from 'morgan'
 import authRoutes from './routes/auth.routes.js'
 import taskRoutes from './routes/tasks.routes.js'
 import cookieParser from 'cookie-parser'
+import { connectDB } from './db.js'
+
+
 
 
 const server = express()
 
-server.use(cors({origin:'http://localhost:5173',credentials:true}))
+server.use(cors({origin:'https://login-project-six-zeta.vercel.app/',credentials:true}))
 server.use(morgan('dev'))
 server.use(express.json())
 server.use(cookieParser())
@@ -17,4 +20,4 @@ server.use("/api",authRoutes)
 server.use("/api",taskRoutes)
 
 
-export default server
+connectDB()
