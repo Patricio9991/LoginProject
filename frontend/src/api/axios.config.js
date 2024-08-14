0c1d990e2e75dccf6c52ee9bcc809639dca94d1e
Patricio9991 <patriciopucheta99@gmail.com>
2024-08-14 12:13:52 -0300

    ready for mongo atals

--- frontend/src/api/axios.config.js
+++ frontend/src/api/axios.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const serverUrl = 'http://localhost:3000'
+const serverUrl = 'mongodb+srv://ppuchetadev:0WCxf2u7foTEDOWJ@mern-landr.htkyrnn.mongodb.net/?retryWrites=true&w=majority&appName=MERN-LandR'
 
 const axiosConfig = axios.create({
     baseURL:`${serverUrl}`,
